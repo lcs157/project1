aafc9adff7aacd0511e262d7ad2c1968627a1bb4
Lucas Massari Andrade <lucasandradesp@hotmail.com>
2024-03-14 10:28:48 -0300

    Removed Contact and About

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
 
 def home(request):
     return render(request,'recipes/home.html', context={
         'name': 'Lucas'
     })
 
-def about(request):
-    return render(request, 'temp/temp.html')
-
-def contact(request):
-    return HttpResponse('contact')
-
-# Create your views here.
