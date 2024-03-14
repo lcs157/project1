9a8fa71148cf1d51cbf264020022fe01f7de39f1
Lucas Massari Andrade <lucasandradesp@hotmail.com>
2024-03-13 23:48:23 -0300

    Template example

--- recipes/views.py
+++ recipes/views.py
@@ -3,10 +3,12 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('HOME 1')
+    return render(request,'recipes/home.html', context={
+        'name': 'Lucas'
+    })
 
 def about(request):
-    return HttpResponse('About')
+    return render(request, 'temp/temp.html')
 
 def contact(request):
     return HttpResponse('contact')
